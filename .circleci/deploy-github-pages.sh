d1c8a3ca34ef57c7e28b1e74f231d0bad1f948d0
ganezasan <istaka12@gmail.com>
2018-11-11 15:48:01 +0900

    update persist_to_workspace

--- .circleci/deploy-github-pages.sh
+++ .circleci/deploy-github-pages.sh
@@ -1,12 +1,11 @@
 git config user.name "$USER_NAME"
 git config user.email "$USER_EMAIL"
 
-find . -maxdepth 1 ! -name '_site' ! -name '.git' ! -name '.gitignore' -exec rm -rf {} \;
+git checkout -b gh-pages origin/gh-pages
+
+find . -maxdepth 1 ! -name '_site' ! -name '.git' ! -name '.gitignore' ! -name 'CNAME' ! -name '.' -exec rm -rf {} \;
 mv _site/* .
 rm -R _site/
-
-git checkout gh-pages
-git pull origin gh-pages
 
 git add -fA
 git commit --allow-empty -m "$(git log master -1 --pretty=%B)"
