f1b2dc7966992463b0ecb5143b9266e5dad059c2
ganezasan <istaka12@gmail.com>
2018-12-29 15:20:49 +0900

    not remove .circleci

--- .circleci/deploy-github-pages.sh
+++ .circleci/deploy-github-pages.sh
@@ -3,9 +3,11 @@ git config user.email "$USER_EMAIL"
 
 git checkout -b gh-pages origin/gh-pages
 
-find . -maxdepth 1 ! -name '_site' ! -name '.git' ! -name '.gitignore' ! -name 'CNAME' ! -name '.' -exec rm -rf {} \;
+find . -maxdepth 1 ! -name '_site' ! -name '.git' ! -name '.gitignore' ! -name 'CNAME' ! -name '.' ! -name '.circleci' -exec rm -rf {} \;
 mv _site/* .
 rm -R _site/
+
+cat .circleci/config.yml
 
 git add -fA
 git commit --allow-empty -m "$(git log master -1 --pretty=%B)"
